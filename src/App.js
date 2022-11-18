6f6a6267a2b9333eb1212bd089d4861398bdc29e
Andres <agodinezvizcardo@gmail.com>
2022-11-17 23:27:24 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Root from "./root";
+import "./assets/css/app.css";
+
+function App() {
+  return <Root />;
+}
+
+export default App;
---
+++ src/root/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Root() {
+  return (
+    <div className="app">
+      <div className="wrapper">
+        <div className="mt-xlarge">
+          <p className="title text-center">Lazy load</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Root;
